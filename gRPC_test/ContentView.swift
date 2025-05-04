81c07dfe9cefcdd48a76a5480a53ad31cead3553
A3426L <mikan7738@gmail.com>
2025-05-04 11:26:11 +0900

    Initial Commit

---
+++ gRPC_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gRPC_test
+//
+//  Created by aru on 2025/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gRPC_test/gRPC_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  gRPC_testApp.swift
+//  gRPC_test
+//
+//  Created by aru on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct gRPC_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
