274d3598be1467939075f79dbdee44a0537b6b8b
A3426L <mikan7738@gmail.com>
2025-05-06 01:24:36 +0900

    [add]swift .proto

--- gRPC_test/ContentView.swift
+++ gRPC_test/ContentView.swift
@@ -6,18 +6,52 @@
 //
 
 import SwiftUI
+import GRPCCore
+import GRPCCodeGen
+import GRPCInProcessTransport
+import NIO
 
 struct ContentView: View {
+    @State private var response = "未送信"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(response)
+            Button("Send Hello") {
+                Task {
+                    await sendHello()
+                }
+            }
         }
-        .padding()
+    }
+
+    func sendHello() async {
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let channel = try! GRPCChannelPool.with(
+            target: .host("127.0.0.1", port: 50051),
+            transportSecurity: .plaintext,
+            eventLoopGroup: group
+        )
+
+        let client = HelloServiceAsyncClient(channel: channel)
+        let request = HelloRequest.with {
+            $0.name = "SwiftUI"
+        }
+
+        do {
+            let reply = try await client.sayHello(request)
+            DispatchQueue.main.async {
+                response = reply.message
+            }
+        } catch {
+            print("RPC error: \(error)")
+        }
+
+        try? await channel.close()
+        try? await group.shutdownGracefully()
     }
 }
+
 
 #Preview {
     ContentView()
